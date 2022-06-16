753e512ebad0e7bcf7dfdf475509c4a2323e9799
Esteban <esteban9223@hotmail.com>
2022-06-16 14:26:18 -0500

    clean up

--- client/teby/vite.config.js
+++ client/teby/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    // jsxInject: `import React from 'react'`,
-    // jsxInject: `import { useState } from 'react'`
-  }
+  plugins: [react()]
 })
